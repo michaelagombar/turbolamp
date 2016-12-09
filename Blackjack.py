bcd012ce602ee411934e800eb62a6071403240a5
michaelagombar <michael.agombar@gmail.com>
2016-12-09 14:01:23 -0600

    blackjack

--- Blackjack.py
+++ Blackjack.py
@@ -76,88 +76,40 @@ class Deck:
 
 def hit():
 
+    c = 'y'
 
-    def hit_add_player():
-        player_hand.add_card(deck.deal_card())
-        print("Player 1: %s" % player_hand)
-        print("The value of player 1's hand is: %s" % player_hand.get_value())
-        if player_hand.get_value() > 21:
-            print("You Busted! You lose! You Suck!")
+    while c != 'q':
+     #   print("Player 1: %s" % player_hand)
 
+      #  print("The value of player 1's hand is: %s" % player_hand.get_value())
 
+       # print("Player 1: %s" % dealer_hand)
+        #print("The value of dealer 1's hand is: %s" % dealer_hand.get_value())
 
-    def hit_add_dealer():
-        dealer_hand.add_card(deck.deal_card())
-        print("Player 1: %s" % dealer_hand)
-        print("The value of dealer 1's hand is: %s" % dealer_hand.get_value())
-        if dealer_hand.get_value() > 21:                #this is screwy - if busted, still asks to hit
-            print("You Busted! You lose! You Suck!")
+        c = input(print("Does Player 1  want to hit? y/n"))
 
+        if c == 'y':
+            player_hand.add_card(deck.deal_card())
+            print("Player 1: %s" % player_hand)
+            print("The value of player 1's hand is: %s" % player_hand.get_value())
 
-    def print_end_player():
-        print("Thanks, Player 1! Your hand is: %s" % player_hand, "And your value is: %s" % player_hand.get_value())
-
-    def print_end_dealer():
-        print("Thanks, DEALER! Your hand is: %s" % dealer_hand, "And your value is: %s" % dealer_hand.get_value())
-
-
-    #this below should probably go into a function, I believe
-
-
-    if player_hand.get_value() <=21:
-        hit_not_p1 = input(print("Does Player 1  want to hit? y/n"))
-        if hit_not_p1 == 'y':
-            hit_add_player()
-
-            hit_not_p2 = input(print("Does Player 1  want to hit? y/n"))
-
-            if hit_not_p2 == 'y' and player_hand.get_value() <=21:
-                hit_add_player()
-
-            elif hit_not_p2 == 'n':
-                print_end_player()
+            if player_hand.get_value() > 21:
+                print("BUST!")
+                new_game = input(print("do you want to play again? y/n"))
+                if new_game == 'y':
+                    deal()
+        elif c == 'n':
+            print("Player 1: %s" % player_hand)
+            print("The value of player 1's hand is: %s" % player_hand.get_value())
+            if player_hand.get_value() > dealer_hand.get_value():
+                print("YOU WIN!!!!!!")
+            else:
+                print("YOU LOSE!!!!")
 
 
-            hit_not_p3 = input(print("Does Player 1  want to hit? y/n"))
-
-            if hit_not_p3 == 'y'  and player_hand.get_value() <=21:
-                hit_add_player()
-
-            elif hit_not_p3 == 'n':
-                print_end_player()
 
 
-        else:
-            print_end_player()
 
-
-    #this could probably, for sure go into a function. i bet theres a way to do this
-
-    if dealer_hand.get_value() <=21:
-        hit_not_d1 = input(print("Does dealer want to hit? y/n"))
-        if hit_not_d1 == 'y':
-            hit_add_dealer()
-
-            hit_not_d2 = input(print("Does dealer  want to hit? y/n"))
-
-            if hit_not_d2 == 'y' and dealer_hand.get_value() <=21:
-                hit_add_player()
-
-            elif hit_not_p2 == 'n':
-                print_end_dealer()
-
-
-            hit_not_d3 = input(print("Does dealer  want to hit? y/n"))
-
-            if hit_not_d3 == 'y'  and dealer_hand.get_value() <=21:
-                hit_add_dealer()
-
-            elif hit_not_d3 == 'n':
-                print_end_dealer()
-
-
-        else:
-            print_end_dealer()
 
 def winner():
 
