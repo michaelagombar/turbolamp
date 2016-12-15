23d5d052e09a0ca849260be544c3d3180407f407
michaelagombar <michael.agombar@gmail.com>
2016-12-15 09:31:10 -0600

    RPS

--- Rock_Paper_Sci.py
+++ Rock_Paper_Sci.py
@@ -12,25 +12,22 @@ def play():
     dec = randint(1, 3)
     user_choice = input("Do you want (p)aper, (r)ock, or (s)cissors?")
 
-
-
     if user_choice == 'r':
 
         if dec == 1:
             print("Player selected ROCK, computer selected ROCK. DRAW!")
             score(player_win_count, computer_win_count)
 
-
         elif  dec == 2:
             print("Computer wins! Player selected ROCK, computer selected PAPER")
             computer_win_count += 1
             score(player_win_count, computer_win_count)
 
-
         elif dec == 3:
             print ("Player wins! Player selected ROCK, computer selected SCISSORS!")
             player_win_count += 1
             score(player_win_count, computer_win_count)
+
 
     if user_choice == 'p':
 
@@ -59,10 +56,12 @@ def play():
             player_win_count += 1
             score(player_win_count, computer_win_count)
 
-
         elif dec == 3:
             print("DRAW! Player selected SCISSORS, computer selected SCISSORS!")
             score(player_win_count, computer_win_count)
+
+    else:
+        play()
 
 
 def score(player_win_count, computer_win_count):
@@ -75,9 +74,11 @@ def play_again(): #this could be an issue moving forward, as I believe there cou
     choice = input("Do you want to play again? y/n")
     if choice == 'y':
         play()
-    else:
+    elif choice == 'n':
         print("OK! The final score was\n Player: {} to Computer: {}".format(player_win_count, computer_win_count))
         sys.exit()
+    else:
+        play_again()
 
 
 
